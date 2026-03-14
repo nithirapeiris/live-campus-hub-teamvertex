031e570507bbc4f52fd050784ae6be13646b48ac
HashenCharuka <139098900+HashenCharuka@users.noreply.github.com>
2026-03-14 13:15:05 +0530

    Add files via upload

---
+++ Scripts.sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2026 at 08:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vertex_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus`
+--
+
+CREATE TABLE `bus` (
+  `bus_id` varchar(10) NOT NULL,
+  `departure` varchar(50) NOT NULL,
+  `arrival` varchar(50) NOT NULL,
+  `departure_time` datetime NOT NULL,
+  `arrival_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `check_in`
+--
+
+CREATE TABLE `check_in` (
+  `checkin_id` varchar(10) NOT NULL,
+  `student_id` varchar(10) NOT NULL,
+  `location_id` varchar(10) NOT NULL,
+  `checkIn_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clubs`
+--
+
+CREATE TABLE `clubs` (
+  `club_id` varchar(10) NOT NULL,
+  `club_name` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `department_id` varchar(10) NOT NULL,
+  `department_Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`department_id`, `department_Name`) VALUES
+('D001', 'Computing'),
+('D002', 'Business'),
+('D003', 'Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` varchar(10) NOT NULL,
+  `event_title` varchar(50) NOT NULL,
+  `event_date` date NOT NULL,
+  `location_id` varchar(10) NOT NULL,
+  `club_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `location_id` varchar(10) NOT NULL,
+  `location_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`location_id`, `location_name`) VALUES
+('L001', 'Zenith'),
+('L002', 'Library'),
+('L003', 'Gym');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rewards`
+--
+
+CREATE TABLE `rewards` (
+  `reward_id` varchar(10) NOT NULL,
+  `reward_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` varchar(10) NOT NULL,
+  `staff_pwd` varchar(25) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` varchar(10) NOT NULL,
+  `student_pwd` varchar(25) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `enrolled_Year` varchar(20) NOT NULL,
+  `department_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `student_pwd`, `first_name`, `last_name`, `phone`, `email`, `enrolled_Year`, `department_id`) VALUES
+('S2409149', 'Pass123!', 'Hashen', 'Charuka', '0722129598', 'charukahashen2@gmail.com', '2023', 'D001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_club`
+--
+
+CREATE TABLE `students_club` (
+  `student_id` varchar(10) NOT NULL,
+  `club_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_reward`
+--
+
+CREATE TABLE `student_reward` (
+  `student_id` varchar(10) NOT NULL,
+  `reward_id` varchar(10) NOT NULL,
+  `earned_date` date NOT NULL,
+  `points` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`bus_id`);
+
+--
+-- Indexes for table `check_in`
+--
+ALTER TABLE `check_in`
+  ADD PRIMARY KEY (`checkin_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `clubs`
+--
+ALTER TABLE `clubs`
+  ADD PRIMARY KEY (`club_id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `location_id` (`location_id`),
+  ADD KEY `club_id` (`club_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `rewards`
+--
+ALTER TABLE `rewards`
+  ADD PRIMARY KEY (`reward_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `students_club`
+--
+ALTER TABLE `students_club`
+  ADD PRIMARY KEY (`student_id`,`club_id`),
+  ADD KEY `club_id` (`club_id`);
+
+--
+-- Indexes for table `student_reward`
+--
+ALTER TABLE `student_reward`
+  ADD PRIMARY KEY (`student_id`,`reward_id`),
+  ADD KEY `reward_id` (`reward_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `check_in`
+--
+ALTER TABLE `check_in`
+  ADD CONSTRAINT `check_in_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  ADD CONSTRAINT `check_in_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`);
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`),
+  ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`club_id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`department_id`);
+
+--
+-- Constraints for table `students_club`
+--
+ALTER TABLE `students_club`
+  ADD CONSTRAINT `students_club_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  ADD CONSTRAINT `students_club_ibfk_2` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`club_id`);
+
+--
+-- Constraints for table `student_reward`
+--
+ALTER TABLE `student_reward`
+  ADD CONSTRAINT `student_reward_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  ADD CONSTRAINT `student_reward_ibfk_2` FOREIGN KEY (`reward_id`) REFERENCES `rewards` (`reward_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
